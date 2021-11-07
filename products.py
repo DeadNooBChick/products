f7fd12a532151b60294f696e513f532d6f7328b3
Kenneth Ku <kennethku324@gmail.com>
2021-11-07 13:06:50 +0800

    add for loop function

--- products.py
+++ products.py
@@ -10,6 +10,10 @@ while True:
 	p = [name, price] #呢個係上三行既coding 簡寫寫法
 	products.append(p) #小清單 P 入 主清單 product
 	# products.append([name, price]) 呢個係第7-11行最簡單寫法
-print(products)
+#print(products)
 
 products [0][0] #product 入面既第0格裡既第0格
+
+for product in products:
+	print(product[0], '的價格是', product[1])
+
